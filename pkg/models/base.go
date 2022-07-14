5eaad13678937f3276f06efcabe8cdb6d297410b
Edward Selin <edwsel@gmail.com>
2022-07-15 00:11:26 +0300

    Fixed base ID typed

--- pkg/models/base.go
+++ pkg/models/base.go
@@ -11,7 +11,7 @@ type BaseIdByUUID struct {
 }
 
 type BaseIdByInt64 struct {
-	Id uuid.UUID `gorm:"column:id;type:BIGINT;autoIncrement;primaryKey"`
+	Id uint64 `gorm:"column:id;type:BIGINT;autoIncrement;primaryKey"`
 }
 
 type Time struct {
