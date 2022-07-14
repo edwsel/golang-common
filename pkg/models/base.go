e38426ab5ffb338657a6b13e1d701923394c8d34
Edward Selin <edwsel@gmail.com>
2022-07-15 00:43:41 +0300

    Fixed base ID typed autoIncrement

--- pkg/models/base.go
+++ pkg/models/base.go
@@ -11,7 +11,7 @@ type BaseIdByUUID struct {
 }
 
 type BaseIdByInt64 struct {
-	Id *uint64 `gorm:"column:id;type:BIGINT;primaryKey;autoIncrementq"`
+	Id *uint64 `gorm:"column:id;type:BIGINT;primaryKey;autoIncrement"`
 }
 
 type Time struct {
